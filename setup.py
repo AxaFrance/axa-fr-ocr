f486a89ad62ba5e14ebb6d533e60d6df0aa44cb0
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2023-07-18 14:47:40 +0200

    fix: updated setup.py to allow publication of the package during CI/CD

--- setup.py
+++ setup.py
@@ -12,6 +12,18 @@ setuptools.setup(
     package_data={"ocr.text": ["*"]},
     install_requires=requirements,
     author="AXA",
+    author_email="guillaume.chervet@axa.fr",
+    url="https://github.com/AxaFrance/axa-fr-ocr",
+    description="AXA France OCR library",
+    long_description="Utility library built on top of Tesseract and providing "
+                     "algorithms to deskew text in order to enhance OCR "
+                     "performance. It also provides an algorithm to determine "
+                     "whether an image contains text.",
+    platforms="POSIX",
+    classifiers=["Programming Language :: Python :: 3 :: Only",
+                 "Programming Language :: Python :: 3.8",
+                 "Topic :: Scientific/Engineering :: Information Analysis",
+                 ],
     include_package_data=True,
     python_requires=">=3.8",
 )
